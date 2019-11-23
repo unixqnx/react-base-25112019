736589bba32f37373fb921ac7744a2ae7523201a
unixqnx <unixqnx@mail.ru>
2019-11-23 20:01:15 +0600

    ReactJS Basics - #5 Outputting dynamic Data

--- src/app/components/Home.js
+++ src/app/components/Home.js
@@ -2,9 +2,17 @@ import React from "react";
 
 export class Home extends React.Component{
     render(){
+        let content = "";
+        if(true){
+            content = <p>Hello!</p>
+        }
+
+
         return(
             <div>
                 <p>In a new component!</p>
+                {content}
+                { 5===7 ? "Yes" : "No" }
             </div>
         );
     }
